7bd1dbd5e965bb5d3ef1f43bd686e1a3b5791b11
Maksim Vinogradov <maxvgrad@gmail.com>
2018-12-22 00:00:19 +0300

    Добавлена сущность AppUserRole

---
+++ tutor-app/src/main/java/ru/maxvgrad/tutor/entity/AppUserRoles.java
@@ -0,0 +1,35 @@
+package ru.maxvgrad.tutor.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppUserRoles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column()
+    private String description;
+
+    @Column()
+    private AppUserRoles parent;
+
+}
