ac80625b3ef14e5da4f65d1a130ad0079de98e33
Maksim Vinogradov <maxvgrad@gmail.com>
2018-12-22 00:41:01 +0300

    Добавлены пустые контроллеры

---
+++ tutor-app/src/main/java/ru/maxvgrad/tutor/controller/AppUserController.java
@@ -0,0 +1,14 @@
+package ru.maxvgrad.tutor.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.maxvgrad.tutor.TutorApplication;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(TutorApplication.MODULE_BASE_URL + "/user")
+public class AppUserController {
+}
