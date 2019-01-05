f6f4ed9fe716a977614aae48c762470187731de2
Maksim Vinogradov <maxvgrad@gmail.com>
2019-01-05 16:55:27 +0200

    Add fields in SubmissionFrom

--- tutor-lib/src/main/java/ru/maxvgrad/tutor/dto/EgeMathProfFormDto.java
+++ tutor-lib/src/main/java/ru/maxvgrad/tutor/dto/EgeMathProfFormDto.java
@@ -1,6 +1,5 @@
 package ru.maxvgrad.tutor.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +7,35 @@ import lombok.NoArgsConstructor;
 import java.io.Serializable;
 
 @Data
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
 public class EgeMathProfFormDto extends SubmissionForm implements Serializable {
+
+    @Builder
+    public EgeMathProfFormDto(Long id, Long examinationFormId, Double b1, Double b2, Double b3, Double b4,
+                              Double b5, Double b6, Double b7, Double b8, Double b9, Double b10, Double b11,
+                              Double b12, Double c1, Double c2, Double c3, Double c4, Double c5, Double c6,
+                              Double c7) {
+        super(id, examinationFormId);
+        this.b1 = b1;
+        this.b2 = b2;
+        this.b3 = b3;
+        this.b4 = b4;
+        this.b5 = b5;
+        this.b6 = b6;
+        this.b7 = b7;
+        this.b8 = b8;
+        this.b9 = b9;
+        this.b10 = b10;
+        this.b11 = b11;
+        this.b12 = b12;
+        this.c1 = c1;
+        this.c2 = c2;
+        this.c3 = c3;
+        this.c4 = c4;
+        this.c5 = c5;
+        this.c6 = c6;
+        this.c7 = c7;
+    }
 
     private Double b1;
 
--- tutor-lib/src/main/java/ru/maxvgrad/tutor/dto/SubmissionForm.java
+++ tutor-lib/src/main/java/ru/maxvgrad/tutor/dto/SubmissionForm.java
@@ -1,4 +1,13 @@
 package ru.maxvgrad.tutor.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class SubmissionForm {
+
+    private Long id;
+
+    private Long examinationFormId;
 }
